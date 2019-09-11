743637b316d774b9e164f810be845b824ffdef4e
Khushal Sawant <47523964+Khushalsawant@users.noreply.github.com>
2019-09-11 08:29:29 +0530

    Add files via upload

--- Extractive_summerization.py
+++ Extractive_summerization.py
@@ -5,8 +5,17 @@ Created on Mon Sep  9 06:57:44 2019
 @author: khushal
 """
 
+
+import time
+# into hours, minutes and seconds 
+import datetime 
+
+start_time = time.time()
+
 import requests
 from bs4 import BeautifulSoup
+
+import wikipedia
 
 import nltk
 import os
@@ -18,6 +27,9 @@ from nltk.tokenize import sent_tokenize,word_tokenize
 from nltk.corpus import stopwords
 Stopwords = set(stopwords.words('english'))
 wordlemmatizer = WordNetLemmatizer()
+
+def convert_sec(n): 
+    return str(datetime.timedelta(seconds = n))
 
 def lemmatize_words(words):
     lemmatized_words = []
@@ -98,83 +110,90 @@ def sentence_importance(sentence,dict_freq,sentences):
                 word = wordlemmatizer.lemmatize(word)
                 sentence_score = sentence_score + word_tfidf(dict_freq,word,sentences,sentence)
      return sentence_score
-'''
-extracted_str = "/n"
-page = requests.get("https://www.investopedia.com/terms/e/etf.asp")
-if page.status_code == 200:
-    #print(page.content)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    #print(soup.prettify())
-    soup.find_all('p')
-    for i in range(len(soup.find_all('p'))):
-        extracted_str = extracted_str + soup.find_all('p')[i].get_text()
-'''
 
 
-import wikipedia
-import requests
-from bs4 import BeautifulSoup
-from nltk.tokenize import sent_tokenize
-
-extracted_str = "/n"
-#print(wikipedia.summary("ETF"))
-ETF = wikipedia.page("ETF")
-print(ETF.url)
-extracted_list = []
-page = requests.get(ETF.url)
-if page.status_code == 200:
-    #print(page.content)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    #print(soup.prettify())
-    soup.find_all('p')
-    for i in range(len(soup.find_all('p'))):
-        extracted_str = extracted_str +soup.find_all('p')[i].get_text()
-
-
-text = extracted_str
-
-tokenized_sentence = sent_tokenize(text)
-print(" Length of tokenized_sentence",len(tokenized_sentence))
-text = remove_special_characters(str(text))
-text = re.sub(r'\d+', '', text)
-tokenized_words_with_stopwords = word_tokenize(text)
-tokenized_words = [word for word in tokenized_words_with_stopwords if word not in Stopwords]
-tokenized_words = [word for word in tokenized_words if len(word) > 1]
-tokenized_words = [word.lower() for word in tokenized_words]
-tokenized_words = lemmatize_words(tokenized_words)
-word_freq = freq(tokenized_words)
-
-input_user = 30 #int(input('Percentage of information to retain(in percent):'))
-print(" Percentage of information to retain(in percent): ",input_user)
-no_of_sentences = int((input_user * len(tokenized_sentence))/100)
-print(" no_of_sentences = ",no_of_sentences)
-c = 1
-sentence_with_importance = {}
-for sent in tokenized_sentence:
-    sentenceimp = sentence_importance(sent,word_freq,tokenized_sentence)
-    sentence_with_importance[c] = sentenceimp
-    c = c+1
-sentence_with_importance = sorted(sentence_with_importance.items(), key=operator.itemgetter(1),reverse=True)
-cnt = 0
-summary = []
-sentence_no = []
-for word_prob in sentence_with_importance:
-    if cnt < no_of_sentences:
-        sentence_no.append(word_prob[0])
+def summarized_data_from_wiki(input_str):
+    extracted_str = "/n"
+    #print(wikipedia.summary("ETF"))
+    ETF = wikipedia.page(input_str)
+    print(ETF.url)
+    extracted_list = []
+    page = requests.get(ETF.url)
+    if page.status_code == 200:
+        
+        spl_chr_list = []
+        extracted_str_list = []
+        for i in range(101):
+            spl_chr = "[" + str(i) + "]"
+            spl_chr_list.append(spl_chr)
+        
+        #print(page.content)
+        
+        soup = BeautifulSoup(page.content, 'html.parser')
+        #print(soup.prettify())
+        soup.find_all('p')
+        for i in range(len(soup.find_all('p'))):
+            extracted_str = extracted_str +soup.find_all('p')[i].get_text()
+        
+        for char in spl_chr_list:
+            if extracted_str.find(char) != -1:
+                extracted_str = extracted_str.replace(str(char),"")
+                extracted_str_list.append(extracted_str)
+    
+    
+    text = extracted_str_list[-1]
+    
+    tokenized_sentence = sent_tokenize(text)
+    #print(" Length of tokenized_sentence",len(tokenized_sentence))
+    
+    text = remove_special_characters(str(text))
+    text = re.sub(r'\d+', '', text)
+    tokenized_words_with_stopwords = word_tokenize(text)
+    tokenized_words = [word for word in tokenized_words_with_stopwords if word not in Stopwords]
+    tokenized_words = [word for word in tokenized_words if len(word) > 1]
+    tokenized_words = [word.lower() for word in tokenized_words]
+    tokenized_words = lemmatize_words(tokenized_words)
+    word_freq = freq(tokenized_words)
+    
+    input_user = 30 #int(input('Percentage of information to retain(in percent):'))
+    print(" Percentage of information to retain(in percent): ",input_user)
+    no_of_sentences = int((input_user * len(tokenized_sentence))/100)
+    print(" no_of_sentences In Extracted data from wiki = ",no_of_sentences)
+    c = 1
+    sentence_with_importance = {}
+    for sent in tokenized_sentence:
+        sentenceimp = sentence_importance(sent,word_freq,tokenized_sentence)
+        sentence_with_importance[c] = sentenceimp
+        c = c+1
+    sentence_with_importance = sorted(sentence_with_importance.items(), key=operator.itemgetter(1),reverse=True)
+    cnt = 0
+    summary = []
+    sentence_no = []
+    for word_prob in sentence_with_importance:
+        if cnt < no_of_sentences:
+            sentence_no.append(word_prob[0])
+            cnt = cnt+1
+        else:
+          break
+    sentence_no.sort()
+    cnt = 1
+    for sentence in tokenized_sentence:
+        if cnt in sentence_no:
+           summary.append(sentence)
         cnt = cnt+1
-    else:
-      break
-sentence_no.sort()
-cnt = 1
-for sentence in tokenized_sentence:
-    if cnt in sentence_no:
-       summary.append(sentence)
-    cnt = cnt+1
-summary = " ".join(summary)
-print("\n")
-print("Summary:")
-print(summary)
-'''
-outF = open('summary.txt',"w")
-outF.write(summary)
-'''
+    summary = " ".join(summary)
+        
+    summary_tokenized_sentence = sent_tokenize(summary)
+    no_of_sentences_in_summary = int((input_user * len(summary_tokenized_sentence))/100)
+
+    return summary,no_of_sentences_in_summary
+
+if __name__ == "__main__": 
+    input_str = str(input('Enter the text/string for which yo want to get summarizd data := '))    
+    summary,no_of_sentences_in_summary = summarized_data_from_wiki(input_str)
+    print("Summary: \n",summary)
+    print(" no_of_sentences In summary data from wiki = ",no_of_sentences_in_summary)
+    
+    n =  time.time() - start_time
+        
+    print("---Execution Time ---",convert_sec(n))
--- text_scarpping.py
+++ text_scarpping.py
@@ -7,10 +7,18 @@ Created on Mon Sep  9 07:07:09 2019
 
 ## https://www.dataquest.io/blog/web-scraping-tutorial-python/
 
+
+import time
+# into hours, minutes and seconds 
+import datetime 
+
+
+start_time = time.time()
+
+'''
 import requests
 from bs4 import BeautifulSoup
 from nltk.tokenize import sent_tokenize
-
 
 extracted_str = "/n"
 page = requests.get("https://www.investopedia.com/terms/e/etf.asp")
@@ -21,39 +29,44 @@ if page.status_code == 200:
     soup.find_all('p')
     for i in range(len(soup.find_all('p'))):
         extracted_str = extracted_str + soup.find_all('p')[i].get_text()
- 
+''' 
 import wikipedia
 import requests
 from bs4 import BeautifulSoup
 from nltk.tokenize import sent_tokenize
 
 extracted_str = "/n"
+extracted_str_list = []
 #print(wikipedia.summary("ETF"))
 ETF = wikipedia.page("ETF")
 print(ETF.url)
 extracted_list = []
 page = requests.get(ETF.url)
+
+spl_chr_list = []
+for i in range(101):
+    spl_chr = "[" + str(i) + "]"
+    spl_chr_list.append(spl_chr)
+#print(spl_chr_list)    
 if page.status_code == 200:
     #print(page.content)
     soup = BeautifulSoup(page.content, 'html.parser')
     #print(soup.prettify())
     soup.find_all('p')
     for i in range(len(soup.find_all('p'))):
-        extracted_str = extracted_str +soup.find_all('p')[i].get_text()
-    if extracted_str.find('[79]') != -1:
-        print(extracted_str.replace("[79]",""))
-    print(len(extracted_str))
+        extracted_str = extracted_str + soup.find_all('p')[i].get_text()
+    #print(len(extracted_str),extracted_str)
+    
+    for char in spl_chr_list:
+        #print("char = ",char)
+        if extracted_str.find(char) != -1:
+            print("char = ",char)
+            extracted_str = extracted_str.replace(str(char),"")
+            extracted_str_list.append(extracted_str)
+            #print(extracted_str.replace(str(char)," "))
+            #break
+        #print(extracted_str)
 
-spl_chr_list = []
-for i in range(101):
-    spl_chr = "[" + str(i) + "]"
-    spl_chr_list.append(spl_chr)
-
-for char in spl_chr_list:
-    print("char = ",char)
-    if extracted_str.find(char) != -1:
-        extracted_str.replace(char,"")
-        print(extracted_str)
-        
+print(extracted_str_list[-1])
 #print(ETF.content.split("."))
 #print(type(ETF.content))
